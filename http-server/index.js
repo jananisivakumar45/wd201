8f4ff78a2d36f982ed6b8cd36a3e9e03e8129a10
jananisivakumar <curlycresent@gmail.com>
2024-10-08 14:32:43 +0530

    Error cleared in Command Line Argument

--- http-server/index.js
+++ http-server/index.js
@@ -1,61 +1,61 @@
 const http = require("http");
 const fs = require("fs");
-const readline = require("readline");
-let portNo;
+const minimist = require("minimist");
+const args = minimist(process.argv.slice(2));
+
+const port = parseInt(args.port)
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
-const lineDetail = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-})
-lineDetail.question(`Enter port number: `, (port) => {
-  portNo = port;
-  //console.log(`port no : ${port}`);
-  lineDetail.close();
 
-  
-fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  homeContent = home;
-});
-
-fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectContent = project;
-});
-
-fs.readFile("registration.html", (err, registration) => {
-  if (err) {
-    throw err;
-  }
-  registrationContent = registration;
-});
-
-  http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      case "/registration":
-        response.write(registrationContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
+  // Read the HTML files after getting the port number
+  fs.readFile("home.html", (err, home) => {
+    if (err) {
+      throw err;
     }
-  })
-  .listen(portNo);
-})
+    homeContent = home;
+  });
+
+  fs.readFile("project.html", (err, project) => {
+    if (err) {
+      throw err;
+    }
+    projectContent = project;
+  });
+
+  fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+  // Create the server after reading files and getting port number
+  http
+    .createServer((request, response) => {
+      let url = request.url;
+      response.writeHead(200, { "Content-Type": "text/html" });
+
+      // Handle routes
+      switch (url) {
+        case "/project":
+          response.write(projectContent);
+          response.end();
+          break;
+        case "/registration":
+          response.write(registrationContent);
+          response.end();
+          break;
+        case "/":
+        default:
+          response.write(homeContent);
+          response.end();
+          break;
+      }
+    })
+    .listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
 
